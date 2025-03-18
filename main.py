affbb8d07bd887322893259b870206dfb3860cf9
misha <misha07.sharma@gmail.com>
2025-03-19 00:17:21 +0530

    Initial Commit: Add MenuBar to GUI

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import QApplication,QLabel,QGridLayout,QMainWindow
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        # Set the title for the GUI App
+        self.setWindowTitle("Student Management System")
+
+        # Add a MenuBar items
+        file_menu_item = self.menuBar().addMenu("File")
+        help_menu_item = self.menuBar().addMenu("Help")
+
+        # Add the subitem  Add Student of File item in MenuBar
+        add_student_action = QAction("Add Student",self)
+        file_menu_item.addAction(add_student_action)
+
+        # Add the subitem  About of Help item in MenuBar
+        about_action = QAction("About",self)
+        help_menu_item.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
+
+
+
+# Routine call for running gui app
+app = QApplication(sys.argv)
+main_window = MainWindow()
+main_window.show()
+sys.exit(app.exec())
